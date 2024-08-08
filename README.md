625c45066fc4fde5cd38a3cb01531f09c839aec8
Clara Ferraz <claraferraz@proton.me>
2024-08-08 19:22:23 -0300

    aula1: estilização

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # ask-me-anything-front
+
+utilizamos o react 19 com typescript, tailwind, react-router-dom
+lucide-react para ícones
+
+instalações:
+
+começar o projeto com vite:
+npm vite@latest
+escolher react e typescript
+
+para o react 19:
+npm install --save-exact react@rc react-dom@rc
+
+no package.json:
+
+"devDependencies": {
+"@types/react": "npm:types-react@rc",
+"@types/react-dom": "npm:types-react-dom@rc"
+},
+"overrides": {
+"@types/react": "npm:types-react@rc",
+"@types/react-dom": "npm:types-react-dom@rc"
+}
+documentação:
+https://react.dev/blog/2024/04/25/react-19-upgrade-guide
+
+instalar npm: -f para forçar a instalação com react 19
+npm i -f
+
+react router dom:
+npm react-router-dom -f
+
+tailwind:
+npm i tailwindcss postcss autoprefixer -D -f
+
+lucide icons:
+npm i lucide-react -f
+
+em caso de erro de importação no código, basta reiniciar o vscode
---
+++ src/pages/room.tsx
@@ -0,0 +1,68 @@
+import { useParams } from "react-router-dom";
+import amaLogo from "../assets/AMA-logo.svg";
+import { Share2, ArrowRight } from "lucide-react";
+import { toast } from "sonner";
+import { Message } from "../components/message";
+
+export function Room() {
+  const { roomId } = useParams();
+  function handleSendMessage() {}
+
+  function handleShareRoom() {
+    const url = window.location.href.toString();
+    if (navigator.share != undefined && navigator.canShare()) {
+      navigator.share({ url });
+    } else {
+      navigator.clipboard.writeText(url);
+      toast.info("URL copiada para área de transferência");
+    }
+  }
+
+  return (
+    <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-4">
+      <div className="flex items-center gap-3 px-3">
+        <img src={amaLogo} alt="AMA logo" className="h-5" />
+
+        <span className="text-sm text-zinc-500 truncate">
+          Código da sala: <span className="text-zinc-300  ">{roomId}</span>
+        </span>
+
+        <button
+          type="submit"
+          onClick={handleShareRoom}
+          className="ml-auto bg-zinc-800 hover:bg-zinc-700 transition-colors text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm"
+        >
+          Compartilhar
+          <Share2 className="size-4" />
+        </button>
+      </div>
+
+      <div className="h-px w-full bg-zinc-900" />
+
+      <form
+        action={handleSendMessage}
+        className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
+      >
+        <input
+          type="text"
+          name="message"
+          placeholder="Qual a sua pergunta?"
+          className="flex-1 bg-transparent text-sm mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
+          autoComplete="off"
+        />
+        <button
+          type="submit"
+          className="bg-orange-400 hover:bg-orange-500 transition-colors text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm"
+        >
+          Criar Pergunta
+          <ArrowRight className="size-4" />
+        </button>
+      </form>
+
+      <ol className="list-decimal list-outside px-3 space-y-8">
+        <Message text="text" amountOfReactions={2} />
+        <Message text="text 2" amountOfReactions={10} answered />
+      </ol>
+    </div>
+  );
+}
