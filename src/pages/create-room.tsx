3c6e4bfd35e8c265053556a9f795f43068054ec9
Clara Ferraz <claraferraz@proton.me>
2024-08-09 15:39:14 -0300

    função criar sala com requisição http

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,12 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import amaLogo from "../assets/AMA-logo.svg";
 import { ArrowRight } from "lucide-react";
+import { createRoom } from "../http/create-room";
+import { toast } from "sonner";
 
 export function CreateRoom() {
   const navigate = useNavigate();
-  function handleCreateRoom(data: FormData) {
+
+  async function handleCreateRoom(data: FormData) {
     const theme = data.get("theme")?.toString();
-    navigate(`/room/${theme}`);
+    if (!theme) {
+      return;
+    }
+
+    try {
+      const { roomId } = await createRoom({ theme });
+      navigate(`/room/${roomId}`);
+    } catch {
+      toast.error("Falha ao criar sala");
+    }
   }
   return (
     <main className="h-screen flex items-center justify-center px-4">
@@ -26,6 +38,7 @@ export function CreateRoom() {
             placeholder="Nome da sala"
             className="flex-1 bg-transparent text-sm mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
             autoComplete="off"
+            required
           />
           <button
             type="submit"
